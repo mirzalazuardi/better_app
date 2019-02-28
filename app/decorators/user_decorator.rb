e1f6e08594bbb685d2699204921f609ce64e4e48
Mirzalazuardi Hermawan <mirzalazuardi@gmail.com>
2019-02-28 17:29:50 +0700

    Add methor user_link in decorator

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,4 +1,7 @@
 class UserDecorator < BaseDecorator
+  def user_link
+    h.link_to self.name, routes.user_path(self)
+  end
   def edit_user_link
     h.link_to "Edit", routes.edit_user_path(self.id)
   end
