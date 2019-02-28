6b871a4fb735691cdf737e4fa2dc8829e6ba0517
Mirzalazuardi Hermawan <mirzalazuardi@gmail.com>
2019-02-28 17:35:30 +0700

    Add method delete_user_link in user decorator

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -5,4 +5,7 @@ class UserDecorator < BaseDecorator
   def edit_user_link
     h.link_to "Edit", routes.edit_user_path(self)
   end
+  def delete_user_link
+    h.link_to "Delete", routes.user_path(self), data: { confirm: 'Are you sure?', method: 'delete' }
+  end
 end
