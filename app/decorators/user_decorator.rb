49b129aef7065b23f3134da38b66016ca5ae048d
Mirzalazuardi Hermawan <mirzalazuardi@gmail.com>
2019-02-28 17:32:32 +0700

    Cut id, simplify edit_user_link in user decorator

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -3,6 +3,6 @@ class UserDecorator < BaseDecorator
     h.link_to self.name, routes.user_path(self)
   end
   def edit_user_link
-    h.link_to "Edit", routes.edit_user_path(self.id)
+    h.link_to "Edit", routes.edit_user_path(self)
   end
 end
