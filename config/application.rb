5cf729121fef91ed6761fccc8cf31871137af930
Mirzalazuardi Hermawan <mirzalazuardi@gmail.com>
2019-03-07 13:07:14 +0700

    Add request spec generators in application.rb

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,8 @@ module BetterApp
       g.test_framework :rspec,
         view_specs: false,
         helper_specs: false,
-        routing_specs: false
+        routing_specs: false,
+        request_specs: false
     end
   end
 end
