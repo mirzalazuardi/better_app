2d19908c5d5b9f9e306783aff49ec15c62e130d3
Mirzalazuardi Hermawan <mirzalazuardi@gmail.com>
2019-03-07 13:06:22 +0700

    Cut fixtures generators config in application.rb

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,6 @@ module BetterApp
 
     config.generators do |g|
       g.test_framework :rspec,
-        fixtures: false,
         view_specs: false,
         helper_specs: false,
         routing_specs: false
