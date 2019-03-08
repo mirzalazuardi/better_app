bf329259e018a8cd9ffba831caaec97437269b2e
Mirzalazuardi Hermawan <mirzalazuardi@gmail.com>
2019-03-08 09:40:54 +0700

    Add user controller spec

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,3 +1,17 @@
 require 'rails_helper'
-RSpec.describe HomeController, type: :controller do
+RSpec.describe UserController, type: :controller do
+  describe '#index' do
+    it 'respond successfully' do
+      get :index
+      expect(response).to be_success
+    end
+    it 'respond in html format' do
+      get :index, format: :html
+      expect(response.content_type).to eq 'text/html'
+    end
+    it 'respond in json format' do
+      get :index, format: :json
+      expect(response.content_type).to eq 'application/json'
+    end
+  end
 end
