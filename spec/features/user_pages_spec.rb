8a76ee53bf801b9c63198ea7bc69939ff1594be1
Mirzalazuardi Hermawan <mirzalazuardi@gmail.com>
2019-03-14 23:08:08 +0700

    Add scenario show exact count

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.feature 'UserPages', type: :feature do
   scenario 'show exact row count user in index page' do
-    create_list :user, 5, :unique_name
+    row_count = 5
+    create_list :user, row_count, :unique_name
 
     visit users_path
     tbody = page.find(:css, 'tbody')
 
-    expect(tbody.all(:css, 'tr').size).to eq 5
+    expect(tbody.all(:css, 'tr').size).to eq row_count
   end
 end
